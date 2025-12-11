edf9df647fee25244fe695a0932403a8b1ece20a
user <user@DESKTOP-CN4A8JP>
2025-12-11 11:13:28 +0900

    1줄추가

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,6 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
 		System.out.println("Hello world");
 	}
 
